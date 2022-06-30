6489f0cc7bc9872544f95d984b33d56a08a6a467
Vyarin <tranvyvivian@gmail.com>
2022-06-30 13:22:18 -0400

    Add 2-4.c

---
+++ The C Programming Language/2-4.c
@@ -0,0 +1,9 @@
+/* Write an alternate version of squeeze(s1,s2) that deletes each character
+   in the string s1 that matches any character in the string s2. */
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
