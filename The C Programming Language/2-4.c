948e4143fcfaa3476635243ff62c0e39e290e2aa
Vyarin <tranvyvivian@gmail.com>
2022-07-01 14:07:23 -0400

    Implement squeeze()

--- The C Programming Language/2-4.c
+++ The C Programming Language/2-4.c
@@ -3,7 +3,19 @@
 
 #include <stdio.h>
 
-int main(void)
+void squeeze(char s1[], char s2[])
 {
-    return 0;
+    int position = 0;
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
+        for (int j = 0; s2[j] != '\0'; j++)
+        {
+            if (s1[i] != s2[j])
+            {
+                s2[position++] = s2[j];
+            }
+        }
+        s2[position] = '\0';
+        position = 0;
+    }
 }
