6033f38edb804ee109dc1fbe6e982ad1294dd185
Vyarin <tranvyvivian@gmail.com>
2022-07-05 10:53:15 -0400

    Swap s1 and s2 to meet exercise requirements

--- The C Programming Language/2-4.c
+++ The C Programming Language/2-4.c
@@ -6,16 +6,16 @@
 void squeeze(char s1[], char s2[])
 {
     int position = 0;
-    for (int i = 0; s1[i] != '\0'; i++)
+    for (int i = 0; s2[i] != '\0'; i++)
     {
-        for (int j = 0; s2[j] != '\0'; j++)
+        for (int j = 0; s1[j] != '\0'; j++)
         {
-            if (s1[i] != s2[j])
+            if (s2[i] != s1[j])
             {
-                s2[position++] = s2[j];
+                s1[position++] = s1[j];
             }
         }
-        s2[position] = '\0';
+        s1[position] = '\0';
         position = 0;
     }
 }
