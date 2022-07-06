5a4f8dd990fdb40492edd60cee5ba863790c1427
Vyarin <tranvyvivian@gmail.com>
2022-07-06 11:29:18 -0400

    Add exercise 3-6 description

---
+++ The C Programming Language/3-6.c
@@ -0,0 +1,12 @@
+/* 
+   Write a version of itoa that accepts three arguments instead of
+   two. The third argument is a minimum field width; the converted number must
+   be padded with blanks on the left if necessary to make it wide enough.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
