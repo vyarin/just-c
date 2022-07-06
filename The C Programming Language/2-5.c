4453619b8cda2b3bc5614aff0e48480b07ab362f
Vyarin <tranvyvivian@gmail.com>
2022-07-06 19:52:13 -0400

    Add exercise description

---
+++ The C Programming Language/2-5.c
@@ -0,0 +1,12 @@
+/*
+  Write the function any (s 1, s2 ), which returns the first location
+  in the string s 1 where any character from the string s2 occurs, or -1 if s 1
+  contains no characters from s2.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
