bb29b6abb5d62a35eca4025916b1c3afec6160e1
Vyarin <tranvyvivian@gmail.com>
2022-07-06 11:32:16 -0400

    Add itoa() function from 3-4.c

--- The C Programming Language/3-6.c
+++ The C Programming Language/3-6.c
@@ -6,7 +6,37 @@
 
 #include <stdio.h>
 
+void itoa(int n, char s[]);
+
 int main(void)
 {
     return 0;
+}
+
+void itoa(int n, char s[])
+{
+    int i, sign;
+
+    sign = n;
+    i = 0;
+    if (sign >= 0)
+    {
+        do
+        {
+            s[i++] = n % 10 + '0';
+        } while ((n /= 10) > 0);
+    }
+    else
+    {
+        do
+        {
+            s[i++] = -(n % -10) + '0';
+        } while (-(n /= 10) > 0);
+    }
+    if (sign < 0)
+    {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s);
 }
