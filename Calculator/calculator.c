40101782960e475ac72ac628ff39dbabcc1da1dc
Vyarin <tranvyvivian@gmail.com>
2022-07-08 10:20:54 -0400

    Group expressions with parentheses

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -106,7 +106,7 @@ int eval(char *expression)
             if (precedence(expression[i]))
             {
                 int curr_op_prec = precedence(expression[i]);
-                while (!is_empty(operators) && operators->value != ')' 
+                while ((!is_empty(operators) && operators->value != ')') 
                        && (precedence(operators->value) >= curr_op_prec || 
                           (precedence(operators->value) == curr_op_prec && curr_op_prec != 4)))
                 {
