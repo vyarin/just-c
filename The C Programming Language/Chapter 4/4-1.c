2f8241dc6c468b05cb8d4a8780d2c6f930c8836c
Vyarin <tranvyvivian@gmail.com>
2022-07-10 14:46:27 -0400

    Update exercise description

--- The C Programming Language/Chapter 4/4-1.c
+++ The C Programming Language/Chapter 4/4-1.c
@@ -1,6 +1,6 @@
 /* 
    Write the function strrindex (s, t), which returns the position
-   of the rightmost occurrence of t in s, or - 1if there is none
+   of the rightmost occurrence of t in s, or -1 if there is none
 */
 
 #include <stdio.h>
