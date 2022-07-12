1b471bde7154294bd14a5efa2c5b20d29eee373e
Vyarin <tranvyvivian@gmail.com>
2022-07-12 14:04:18 -0400

    Add exercise description

--- The C Programming Language/Chapter 4/4-2.c
+++ The C Programming Language/Chapter 4/4-2.c
@@ -1,3 +1,10 @@
+/*
+   Extend atof to handle scientific notation of the form
+        123.45e-6
+   where a floating-point number may be followed by e or E 
+   and an optionally signed exponent
+*/
+
 #include <stdio.h>
 
 int main(void)
