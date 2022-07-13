566488c16b5bafb87c14e0a99e842e972dad834e
Vyarin <tranvyvivian@gmail.com>
2022-07-13 10:59:06 -0400

    Move bracket

--- The C Programming Language/Chapter 4/4-2.c
+++ The C Programming Language/Chapter 4/4-2.c
@@ -49,7 +49,6 @@ double atof(char s[])
     if (s[i] == 'e' || s[i] == 'E')
     {
         i++;
-    }
         exp_sign = (s[i] == '-') ? -1 : 1;
         if (s[i] == '+' || s[i] == '-')
         {
