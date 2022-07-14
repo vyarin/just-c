3f6ed7579cf522746cdb3e7a4e2706adfaa65151
Vyarin <tranvyvivian@gmail.com>
2022-07-14 11:17:06 -0400

    Add exercise description

--- The C Programming Language/Chapter 4/4-3.c
+++ The C Programming Language/Chapter 4/4-3.c
@@ -1,3 +1,8 @@
+/*
+    Given the basic framework, it's straightforward to extend the calculator. 
+    Add the modulus (%) operator and provisions for negative numbers.
+*/
+
 #include <stdio.h>
 
 int main(void)
