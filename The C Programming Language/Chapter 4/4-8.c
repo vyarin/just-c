58713640d1486fd9caf2e1b793cd9e5c4fefd5ec
Vyarin <tranvyvivian@gmail.com>
2022-07-27 10:44:12 -0400

    Add getch() and ungetch()

---
+++ The C Programming Language/Chapter 4/4-8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define BUFFSIZE 100
+
+int getch(void);
+void ungetch(int);
+void ungets(char s[]);
+
+char buf[BUFFSIZE]; /* buffer for ungetch */
+int bufp = 0;       /* next free position in buf */
+
+int main(void)
+{
+    return 0;
+}
+
+int getch(void) /* get a (possibly pushed back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push back on input */
+{
+    if (bufp >= BUFFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
